6f74227c83c73820a417d3eee458e21beb895049
Kelhann2608 <bernardin2608@gmail.com>
2023-06-23 15:50:39 +0200

    page mentionslegales created and added content in footer + css

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,12 +1,38 @@
 import React from "react";
+import Link from "next/link";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
   return (
-    
-    <footer className=" w-full h-20 p-3 mt-auto text-xl text-center bg-fuchsia-300">
-      ©️ 2023 Secret'Aria
+    <footer className="w-full p-3 mt-auto text-xl text-center bg-fuchsia-300">
+      <div className="grid grid-cols-1 md:grid-cols-3">
+        <div className="pb-3">
+          <p className="text-lg font-semibold text-fuchsia font-DancingS md:text-2xl">
+            <span className="text-2xl underline ">Disponibiltés:</span> Lundi,
+            Mardi,
+            <br />
+            Jeudi de 10h à 12h30 et <br /> de 14h à 18h
+          </p>
+        </div>
+        <div className="pb-3">
+          <p className="text-xl md:text-2xl font-DancingS">
+            {" "}
+            <span className=" md:text-2xl">©️ {currentYear} </span>
+            <span className="font-semibold text-fuchsia-500">Secret'Aria </span>
+            <br />- Tous droits réservés
+          </p>
+        </div>
+
+        <div className="pb-3">
+          <Link
+            href="/mentionslegales"
+            className="items-center font-semibold underline font-DancingS md:text-2xl"
+          >
+            Mentions légales et politique de confidentialité
+          </Link>
+        </div>
+      </div>
     </footer>
-    
   );
 };
 
---
+++ pages/mentionslegales.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Layout } from "@/components/Layout";
+
+function mentionslegales() {
+  return (
+    <Layout>
+      <div className="px-4 pt-20 text-justify">
+        <h1 className="text-2xl font-semibold underline font-DancingS">Mentions légales</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum veritatis illo doloribus. Molestias quibusdam vitae eius exercitationem optio modi cumque?</p>
+        <h1 className='text-2xl font-semibold underline font-DancingS'>Politique de confidentialité</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam sint magni veritatis optio tempora! Nobis ipsum quos id fuga quidem!</p>
+      </div>
+    </Layout>
+  )
+}
+
+export default mentionslegales
