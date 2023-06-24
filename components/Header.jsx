96505bc65f3a97e49bbc1dc1a30832b6637ae417
Kelhann2608 <bernardin2608@gmail.com>
2023-06-24 18:25:52 +0200

    added active link

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,13 @@
 import Link from "next/link";
 import Image from "next/image";
 import { useState } from "react";
+import { usePathname } from "next/navigation";
 
 export const Header = () => {
   const [navbar, setNavbar] = useState(false);
+  const currentRoute = usePathname();
+  const activeLink = "text-white";
+  const normalLink = "";
   return (
     <div>
       <nav className="fixed top-0 left-0 right-0 z-10 w-full bg-fuchsia-300">
@@ -50,22 +54,44 @@ export const Header = () => {
             >
               <ul className="items-center justify-center h-screen md:h-auto md:flex ">
                 <li className="py-3 pb-6 text-2xl text-center border-b-2 border-fuchsia-900 text-fuchsia-500 font-DancingS md:px-6 md:border-b-0 hover:bg-fuchsia-900 md:hover:text-fuchsia-600 md:hover:bg-transparent lg:text-4xl 2xl:text-4xl md:text-2xl">
-                  <Link href="/" onClick={() => setNavbar(!navbar)}>
+                  <Link
+                    href="/"
+                    onClick={() => setNavbar(!navbar)}
+                    className={currentRoute === "/" ? activeLink : normalLink}
+                  >
                     Accueil
                   </Link>
                 </li>
                 <li className="px-6 py-3 pb-6 text-2xl text-center border-b-2 border-fuchsia-900 text-fuchsia-500 font-DancingS md:border-b-0 hover:bg-fuchsia-900 md:hover:text-fuchsia-600 md:hover:bg-transparent lg:text-4xl 2xl:text-4xl md:text-2xl">
-                  <Link href="/about" onClick={() => setNavbar(!navbar)}>
+                  <Link
+                    href="/about"
+                    onClick={() => setNavbar(!navbar)}
+                    className={
+                      currentRoute === "/about" ? activeLink : normalLink
+                    }
+                  >
                     A propos
                   </Link>
                 </li>
                 <li className="px-6 py-3 pb-6 text-2xl text-center border-b-2 border-fuchsia-900 text-fuchsia-500 font-DancingS md:border-b-0 hover:bg-fuchsia-900 md:hover:text-fuchsia-600 md:hover:bg-transparent lg:text-4xl 2xl:text-4xl md:text-2xl">
-                  <Link href="/services" onClick={() => setNavbar(!navbar)}>
+                  <Link
+                    href="/services"
+                    onClick={() => setNavbar(!navbar)}
+                    className={
+                      currentRoute === "/services" ? activeLink : normalLink
+                    }
+                  >
                     Services
                   </Link>
                 </li>
                 <li className="px-6 py-3 pb-6 text-2xl text-center border-b-2 border-fuchsia-900 text-fuchsia-500 font-DancingS md:border-b-0 hover:bg-fuchsia-900 md:hover:text-fuchsia-600 md:hover:bg-transparent lg:text-4xl 2xl:text-4xl md:text-2xl">
-                  <Link href="/tarifs" onClick={() => setNavbar(!navbar)}>
+                  <Link
+                    href="/tarifs"
+                    onClick={() => setNavbar(!navbar)}
+                    className={
+                      currentRoute === "/tarifs" ? activeLink : normalLink
+                    }
+                  >
                     Tarifs
                   </Link>
                 </li>
